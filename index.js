67f5855060ffa3a139c360c6fe2e2d8d8747bdee
rickcarvlh <ricardo.carvlh84@gmail.com>
2021-02-21 12:00:47 +0000

    3 files modified

--- index.js
+++ index.js
@@ -5,8 +5,35 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-// var: block scoped variables -> function scoped
+// var:  scoped variables -> function scoped
 for (var i = 0; i < 10; i++) {
   console.log("var i: ", i);
 }
-console.log("var i:", i);
+console.log("var i , outside of for loop:", i);
+
+// another example
+{
+  var user = "Ryan";
+}
+console.log(user);
+
+// using let
+/*
+{
+  let letUser = "Ryan";
+}
+console.log("let: ", letUser);*/
+
+// TEMPLATE LITARALS
+let name = "ryan";
+let job = "Instructor";
+
+// old
+let setence = name + " works at HackerYou as an " + job;
+console.log(setence);
+
+// template literals
+let namev2 = `Ryan`;
+let jobv2 = `Instructor`;
+let sentencev2 = `${namev2} works at HackerYou as an ${jobv2}`;
+console.log(sentencev2);
